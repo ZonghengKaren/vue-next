62f6457debfe7855720363c58bb2718625965bbd
Zongheng <zonghengkaren@163.com>
2020-08-10 10:07:24 +0800

    ~~~~~~

---
+++ src/util/config.js
@@ -0,0 +1,12 @@
+
+// 接口路径
+export const apiUrl = 'https://testapi.sosotec.com';
+
+// 渠道
+export const srchn = 194000;
+
+// 平台
+export const plat = 19;
+
+// 密匙
+export const signKey = 'xZVrdQniL0pH5I7m8etckbB3azCFs2hT';
